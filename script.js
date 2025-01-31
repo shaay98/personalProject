16e7b932671e646eaeb6c8bef70bd8b4f4767190
shaay98 <nrichardson567@icloud.com>
2025-01-31 21:37:24 +0000

    project

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 "use strict"
 
+document.getElementById('readybtn').addEventListener ('click', function(){
+alert ("You must submit a completed form to adopt.");
+});
---
+++ scripttwo.js
@@ -0,0 +1,5 @@
+"use strict"
+
+document.getElementById('submitbtn').addEventListener('click', function(){
+    alert ("Thank you for submitting your form, please check your email for further instructions.");
+});
